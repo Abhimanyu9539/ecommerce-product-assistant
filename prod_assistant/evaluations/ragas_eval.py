810b617b04597a2ee09f8343353492669f132366
Abhimanyu <iabhimanyu31@gmail.com>
2025-11-23 23:00:00 +0530

    Add RAGAS Eval

--- prod_assistant/evaluations/ragas_eval.py
+++ prod_assistant/evaluations/ragas_eval.py
@@ -4,17 +4,51 @@ from ragas.embeddings import LangchainEmbeddingsWrapper
 from ragas.metrics import LLMContextPrecisionWithoutReference, ResponseRelevancy
 from utils.model_loader import ModelLoader
 import grpc.experimental.aio as grpc_aio
+import asyncio
+from exception.custom_exception import ProductAssistantException
 grpc_aio.init_grpc_aio()
 
 model_loader = ModelLoader()
 
 
-def evaluate_context_precision():
-    """
-    """
-    pass
+def evaluate_context_precision(query, response, retrieved_context):
+    try:
+        sample = SingleTurnSample(
+            user_input=query,
+            response=response,
+            retrieved_contexts=retrieved_context,
+        )
 
-def evaluate_response_relevancy():
-    """
-    """
-    pass
+        async def main():
+            llm = model_loader.load_llm()
+            evaluator_llm = LangchainLLMWrapper(llm)
+            context_precision = LLMContextPrecisionWithoutReference(llm=evaluator_llm)
+            result = await context_precision.single_turn_ascore(sample)
+            return result
+
+        return asyncio.run(main())
+    except Exception as e:
+        #log.error("Error evaluating context precision", error=str(e))
+        raise ProductAssistantException("Failed to evaluate context precision", e)
+
+def evaluate_response_relevancy(query, response, retrieved_context):
+    try:
+        sample = SingleTurnSample(
+            user_input=query,
+            response=response,
+            retrieved_contexts=retrieved_context,
+        )
+
+        async def main():
+            llm = model_loader.load_llm()
+            evaluator_llm = LangchainLLMWrapper(llm)
+            embedding_model = model_loader.load_embeddings()
+            evaluator_embeddings = LangchainEmbeddingsWrapper(embedding_model)
+            scorer = ResponseRelevancy(llm=evaluator_llm, embeddings=evaluator_embeddings)
+            result = await scorer.single_turn_ascore(sample)
+            return result
+
+        return asyncio.run(main())
+    except Exception as e:
+        #log.error("Error evaluating response relevancy", error=str(e))
+        raise ProductAssistantException("Failed to evaluate response relevancy", e)
--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -5,6 +5,7 @@ from utils.model_loader import ModelLoader
 from dotenv import load_dotenv  
 from langchain_classic.retrievers.document_compressors import LLMChainFilter
 from langchain_classic.retrievers import ContextualCompressionRetriever
+from evaluations.ragas_eval import evaluate_response_relevancy, evaluate_context_precision
 
 class Retriever:
     def __init__(self):
@@ -73,7 +74,7 @@ class Retriever:
             compressor = LLMChainFilter.from_llm(llm=llm)
             self.retriever = ContextualCompressionRetriever(
                 base_compressor=compressor,
-                retriever=mmr_retriever
+                base_retriever=mmr_retriever
             )
             print("Retriever loaded successfully")
             return self.retriever
@@ -94,9 +95,33 @@ class Retriever:
 
     
 if __name__ == "__main__":
+    user_query = "Can you tell me Samsung Galaxy S24?"
     retriever_obj  =  Retriever()
-    user_query = "Can you tell me laptop?"
-    results = retriever_obj.call_retriever(user_query)
+    retrieved_docs = retriever_obj.call_retriever(user_query)
 
-    for idx, doc in enumerate(results, 1):
-        print(f"Result {idx} : {doc.page_content} \n Metadata: {doc.metadata} \n")
+    def _format_docs(docs) -> str:
+            if not docs:
+                return "No relevant documents found."
+            formatted_chunks = []
+            for d in docs:
+                meta = d.metadata or {}
+                formatted = (
+                    f"Title: {meta.get('product_title', 'N/A')}\n"
+                    f"Price: {meta.get('price', 'N/A')}\n"
+                    f"Rating: {meta.get('rating', 'N/A')}\n"
+                    f"Reviews:\n{d.page_content.strip()}"
+                )
+                formatted_chunks.append(formatted)
+            return "\n\n---\n\n".join(formatted_chunks)
+        
+    retrieved_contexts = [_format_docs(doc) for doc in retrieved_docs]
+    
+    #this is not an actual output this have been written to test the pipeline
+    response="iphone 16 plus, iphone 16, iphone 15 are best phones under 1,00,000 INR."
+    
+    context_score = evaluate_context_precision(user_query,response,retrieved_contexts)
+    relevancy_score = evaluate_response_relevancy(user_query,response,retrieved_contexts)
+    
+    print("\n--- Evaluation Metrics ---")
+    print("Context Precision Score:", context_score)
+    print("Response Relevancy Score:", relevancy_score)
