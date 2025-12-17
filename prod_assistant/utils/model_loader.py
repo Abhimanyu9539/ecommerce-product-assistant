db0317a1b15b2e69903b063f8063fa9477d26499
Abhimanyu <iabhimanyu31@gmail.com>
2025-12-18 00:21:01 +0530

    Ready for deployment

--- prod_assistant/utils/model_loader.py
+++ prod_assistant/utils/model_loader.py
@@ -4,7 +4,7 @@ import json
 from dotenv import load_dotenv
 from utils.config_loader import load_config
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_groq import ChatGroq
 from logger import GLOBAL_LOGGER as log
 from exception.custom_exception import ProductAssistantException
@@ -84,9 +84,9 @@ class ModelLoader:
             except RuntimeError:
                 asyncio.set_event_loop(asyncio.new_event_loop())
 
-            return GoogleGenerativeAIEmbeddings(
+            return OpenAIEmbeddings(
                 model=model_name,
-                google_api_key=self.api_key_mgr.get("GOOGLE_API_KEY")  # type: ignore
+                openai_api_key=self.api_key_mgr.get("OPENAI_API_KEY")  # type: ignore
             )
         except Exception as e:
             log.error("Error loading embedding model", error=str(e))
