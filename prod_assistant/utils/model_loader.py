54620899429947b033b7b6974dbcf99ae689eed1
Abhimanyu <iabhimanyu31@gmail.com>
2025-12-17 23:54:50 +0530

    Add google key

--- prod_assistant/utils/model_loader.py
+++ prod_assistant/utils/model_loader.py
@@ -12,7 +12,7 @@ import asyncio
 
 
 class ApiKeyManager:
-    REQUIRED_KEYS = [ "OPENAI_API_KEY"]
+    REQUIRED_KEYS = ["GOOGLE_API_KEY", "OPENAI_API_KEY"]
 
     def __init__(self):
         self.api_keys = {}
